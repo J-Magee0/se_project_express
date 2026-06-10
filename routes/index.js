c32606d3460e286f881ff0bff6bc963592f15abb
JELIN MAGEE <jelin.magee0@gmail.com>
2026-06-09 23:36:43 -0600

    update index.js notfounderror to class

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,11 @@ const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemsRouter = require("./clothingItems");
 const { createUser, login } = require("../controllers/users");
-const { NOT_FOUND_ERROR } = require("../utils/errors");
+const { NotFoundError } = require("../utils/errors");
 
 // Public routes
 router.post("/signup", createUser);
 router.post("/signin", login);
-
 
 // Protected routes
 router.use("/users", userRouter);
@@ -15,7 +14,7 @@ router.use("/items", clothingItemsRouter);
 
 // 404 handler
 router.use((req, res) => {
-  res.status(NOT_FOUND_ERROR).send({ message: "Requested resource not found" });
+  res.status(NotFoundError).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
