0286e4deb1012fc51fd518725dc66750addeccfb
JELIN MAGEE <jelin.magee0@gmail.com>
2026-06-19 12:14:16 -0600

    Impplement helmet middleware and rate limit to protect against Dos attacks

--- app.js
+++ app.js
@@ -2,28 +2,25 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const mongoose = require("mongoose");
 const { errors } = require("celebrate");
+const rateLimit = require("express-rate-limit");
 const mainRouter = require("./routes/index");
 const errorHandler = require("./middlewares/err-handler");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
+const limiter = require("./utils/rateLimitConfig");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 // Middlewares
+app.use(helmet());
+app.use(limiter);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
 app.use(requestLogger);
-
-app.use((req, res, next) => {
-  req.user = {
-    _id: "5d8b8592978f8bd833ca8133",
-  };
-  next();
-});
 
 app.get("/crash-test", () => {
   setTimeout(() => {
